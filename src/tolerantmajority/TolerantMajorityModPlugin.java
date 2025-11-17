03e44bca1e0a76e3710757817715a34570a76091
exon-farer <exonexpedition@gmail.com>
2025-11-17 20:21:30 +0800

    Official Release.

---
+++ src/tolerantmajority/TolerantMajorityModPlugin.java
@@ -0,0 +1,27 @@
+package tolerantmajority;
+
+import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.listeners.ColonySizeChangeListener;
+import tolerantmajority.campaign.TolerantLuddicMajorityListener;
+
+public class TolerantMajorityModPlugin extends BaseModPlugin {
+
+    @Override
+    public void onGameLoad(boolean newGame) {
+        // Add listener if not present
+        if (!Global.getSector().getListenerManager().hasListenerOfClass(TolerantLuddicMajorityListener.class)) {
+            Global.getSector().getListenerManager().addListener(new TolerantLuddicMajorityListener());
+        }
+
+        // Remove vanilla Luddic Church size-change behavior
+        for (ColonySizeChangeListener l :
+                Global.getSector().getListenerManager().getListeners(ColonySizeChangeListener.class)) {
+
+            if (l instanceof com.fs.starfarer.api.impl.campaign.intel.events.LuddicChurchHostileActivityFactor) {
+                Global.getSector().getListenerManager().removeListener(l);
+            }
+        }
+    }
+
+}
